9d94fea76208d7c8788e65a78e4869af631f8721
= <=>
2024-07-12 13:24:22 +0000

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\TodoController;
+use App\Models\Todo;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/' , [TodoController::class,'index']);
+Route::post('/', [TodoController::class, 'store']);
+Route::patch('/{todo}', [TodoController::class,'update']);
+Route::delete('/{todo}', [TodoController::class,'destroy']);
+
